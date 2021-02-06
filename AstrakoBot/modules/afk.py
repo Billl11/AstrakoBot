063d1fc5b9da78aff8cb63a931ef500991c50b62
muklas <76889639+muklasss@users.noreply.github.com>
2021-02-06 17:29:26 +0700

    Update afk.py

--- AstrakoBot/modules/afk.py
+++ AstrakoBot/modules/afk.py
@@ -132,7 +132,7 @@ def check_afk(update, context, user_id, fst_name, userc_id):
             res = "{} Sedang OFFLINE".format(fst_name)
             update.effective_message.reply_text(res)
         else:
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} Sedang OFFLINE.\nKarena: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
